4e6cc003a3dd06008f7911d3f1d26f4e7d267bca
Ana Carolina Manzan <ana.manzan@dextra-sw.com>
2020-02-24 17:29:54 -0300

    Added entity to Domain project

---
+++ src/Validations.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace Validations.Domain.Entities
+{
+    public class User
+    {
+        public string Name {get;set;}
+        public string Email {get;set;}
+        public string PhoneNumber {get;set;}
+    }
+}
